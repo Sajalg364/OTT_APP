e8349b58166caff2316fb485af72fe5d31adc49a
Sajalg364 <sajalmahajan364@gmail.com>
2023-10-09 12:02:41 +0530

    First Commit

---
+++ theme/index.js
@@ -0,0 +1,11 @@
+// you can change these colors to change the look of the app ;)
+export const theme = {
+   // background: '#eab308',
+   background: 'red',
+   // text: '#eab308'
+   text: 'red'
+}
+export const styles = {
+   text: {color: theme.text},
+   background: {backgroundColor: theme.background}
+}
